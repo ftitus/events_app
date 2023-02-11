4a0c5f034b8070a837160478430b0587606c2a69
ftitus <ftitus@users.noreply.github.com>
2023-02-11 00:44:58 -0800

    update image size

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -7,7 +7,7 @@ const EventsPage = ({ data }) => {
       <div>
         {data.map((ev) => (
           <a key={ev.id} href={"/events/${ev.id}"}>
-            <Image src={ev.image} alt={ev.title} width={300} height={100} />
+            <Image src={ev.image} alt={ev.title} width={300} height={300} />
             <h2> {ev.title}</h2>
           </a>
         ))}

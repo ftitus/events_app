5f62e434f591ccaed39f7b240b5ddb987960e451
ftitus <ftitus@users.noreply.github.com>
2023-02-11 22:18:37 -0800

    begin refactoring for UI design

--- src/pages/events/[cat]/[id].js
+++ src/pages/events/[cat]/[id].js
@@ -1,4 +1,41 @@
-const EventPage = () => {
-  return <h1> Our Single Event </h1>;
+const EventPage = ({ data }) => {
+  console.log(data);
+  return (
+    <div>
+      <Image src={data.image} width={1000} height={500} alt={data.title} />{" "}
+      <h1>{data.ttitle}</h1>
+      <p>{data.description}</p>
+    </div>
+  );
 };
 export default EventPage;
+
+export async function getStaticPaths() {
+  const data = import("/data/data.json");
+  const allEvents = data;
+
+  const allPaths = allEvents.map((path) => {
+    return {
+      params: {
+        cat: path.city,
+        id: path.id,
+      },
+    };
+  });
+
+  return {
+    paths: allPaths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  console.log(context);
+  const id = context.params.id;
+  const { allEvents } = await import("/data/data.json");
+  const eventData = allEvents.find((ev) => id === ev.id);
+
+  return {
+    props: { data: eventData },
+  };
+}
